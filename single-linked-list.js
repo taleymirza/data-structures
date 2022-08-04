8b2e4b545de193d28ca2089b0454b59a304175b4
Taley'a Mirza <34168281+taleymirza@users.noreply.github.com>
2022-08-04 11:30:01 +0200

    feat: v1

---
+++ single-linked-list.js
@@ -0,0 +1,109 @@
+
+var Node = function(val, next=null) {
+    this.val = val;
+    this.next = next
+    
+};
+
+var MyLinkedList = function() {
+    this.head = null;
+    this.count = 0
+    
+};
+
+/** 
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+    if(this.count===0) return -1
+    
+    var current = this.head, counter = 0
+    while(counter!==index){
+        current = current.next
+        counter++
+    }
+    return current.val
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+    var node = new Node(val)
+    this.head = node
+    this.count++
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+    
+    if(this.count===0) MyLinkedList.addAtHead(val)
+    else{
+        var current = this.head
+        var node = new Node(val)
+        while(current.next!==null){
+            current= current.next
+        }
+        current.next = node;
+        this.count++
+    }
+   
+    
+};
+
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+    if(index===0) this.addAtHead(val)
+    else if (index===this.count) this.addAtTail(val)
+    else{
+        var counter = 0, previous = this.head
+        while(counter!==index-1){
+            previous = previous.next
+            counter++
+        }
+        let currentAtIndex = previous.next
+        var node= new Node(val, currentAtIndex)
+        previous.next = node
+        this.count++
+    }
+    
+};
+
+/** 
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+    if(index>=0 && index<this.count){
+        var counter = 0, previous = this.head
+        while(counter!==index-1){
+            previous = previous.next
+            counter++
+        }
+        var currentAtIndex = previous.next
+        previous.next = currentAtIndex.next
+        this.count--
+    }
+    
+};
+
+/** 
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
